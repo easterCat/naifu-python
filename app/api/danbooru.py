14d24777f49906aa94b76a7759cd1e37c4e36d44
aldrich <jslilin987654321@gmail.com>
2023-08-23 17:52:36 +0800

    feat: 添加tag提示

--- app/api/danbooru.py
+++ app/api/danbooru.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import urllib.request
 import uuid
 
 import requests
@@ -78,58 +77,88 @@ class DanbooruAnalysis(Resource):
 
 @ns.route("/list")
 class BooruList(Resource):
-    # https://github.com/yukishirataco/animepics-pest/blob/master/start.py
     @staticmethod
     def get():
-        parser.add_argument("searchText", help="请输入你要查询的tag(可以有多个)", type=str, default='')
+        parser.add_argument("searchText", help="请输入你要查询的tag(可以有多个)", type=str, default='all')
         parser.add_argument("pageSize", help="每页多少个", type=int, default=100)
         parser.add_argument("pageIndex", help="第几页", type=int, default=1)
         params = parser.parse_args()
-        pageSize = params['pageSize']
+        page_size = params['pageSize']
+        page_index = params['pageIndex']
         tags = params['searchText']
         rate = ' '
         source = r'http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags='
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
         url = source + tags + rate
-        r = requests.get(url, headers=headers)
-        opener = urllib.request.build_opener()
-        opener.addheaders = [
-            (
-                'User-Agent',
-                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36'
-            )
-        ]
-        urllib.request.install_opener(opener)
-        d_list = []
-        if r.status_code == 200:
-            soup = BeautifulSoup(r.text, "lxml")
-            num = int(soup.find('posts')['count'])
-            max_page = int(round(num / pageSize))
-            # 查看页面上有多少图片，以100张图一页的话有多少图
-            count = 0
-            logger.info(
-                url + 'API访问正常...\n对于tag:' + tags + rate + ',有' + str(num) + '张图，' + str(max_page) + '页')
-            if num == 0:
-                logger.error('没有找到图片，可能是出错了...')
-                return {"code": 500, "msg": "没有找到图片，可能是出错了...", "data": ""}, 200
-            else:
-                page = params['pageIndex'] - 1
-                # 爬取所有页面的图，所以在每一页都要找到图片
-                pages = requests.get(url + '&pid=' + str(page))
-                if pages.status_code == 200:
-                    get_page_xml = BeautifulSoup(pages.text, "lxml")
-                    posts = get_page_xml.find('posts')
-                    pics = posts.find_all('post')
-                    for link in pics:
-                        d_list.append({
-                            "id": link.find("id").get_text(),
-                            "minify_preview": link.find("preview_url").get_text(),
-                            "preview": link.find("file_url").get_text(),
-                            "name": link.find("image").get_text(),
-                            "sample": link.find("sample").get_text(),
-                            "prompt": link.find("tags").get_text(),
-                        })
-                        count = count + 1
-                    return {"code": 200, "msg": "解析成功", "data": {'list': d_list, 'total': num}}, 200
+        response = requests.get(url, headers=headers)
+        result_list = []
+
+        if response.status_code != 200:
+            return {"code": 500, "msg": "gelbooru请求出现错误", "data": ""}, 200
+
+        soup = BeautifulSoup(response.text, "lxml")
+        total_count = int(soup.find('posts')['count'])
+        total_page = int(round(total_count / page_size))
+
+        if total_count == 0:
+            logger.error('没有找到图片，可能是出错了...')
+            return {"code": 500, "msg": "没有找到图片，可能是出错了...", "data": ""}, 200
+
+        logger.info(
+            f"{url} API访问正常...\n对于tag:{tags}{rate}，有{total_count}张图，{total_page}页"
+        )
+
+        page = page_index - 1
+        page_response = requests.get(url + '&pid=' + str(page))
+        if page_response.status_code == 200:
+            get_page_xml = BeautifulSoup(page_response.text, "lxml")
+            posts = get_page_xml.find('posts')
+            pics = posts.find_all('post')
+            for link in pics:
+                result_list.append({
+                    "id": link.find("id").get_text(),
+                    "minify_preview": link.find("preview_url").get_text(),
+                    "preview": link.find("file_url").get_text(),
+                    "name": link.find("image").get_text(),
+                    "sample": link.find("sample").get_text(),
+                    "prompt": link.find("tags").get_text(),
+                })
+            return {"code": 200, "msg": "解析成功", "data": {'list': result_list, 'total': total_count}}, 200
+
+
+@ns.route("/tags")
+class BooruTags(Resource):
+    @staticmethod
+    def get():
+        parser.add_argument("searchText", help="请输入你要查询的tag(可以有多个)", type=str, default='all')
+        params = parser.parse_args()
+        search_text = params['searchText']
+        source = r'http://gelbooru.com/index.php?page=tags&s=list&sort=desc&order_by=index_count'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
+        if search_text != '':
+            url = f"{source}&tags={search_text}"
         else:
-            return {"code": 500, "msg": "gelbooru出现错误", "data": ""}, 200
+            url = source
+        result_list = []
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            return {"code": 500, "msg": "获取booru的tags失败", "data": {"tags": result_list, "total": 0}}, 200
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        table = soup.select_one('table.highlightable')
+        rows = table.find_all('tr')
+        for row in rows:
+            first_td = row.select_one('td:first-child')
+            if first_td is None:
+                continue
+            span_type = first_td.select_one('a').text
+            span_count = row.select_one('span.tag-count').text
+            if span_type == '':
+                continue
+            result_list.append({
+                "tag_name": span_type.replace(" ", "_"),
+                "tag_count": span_count
+            })
+
+        return {"code": 200, "msg": "获取tags成功", "data": {"tags": result_list, "total": len(result_list)}}, 200
